49fc0f695674a96dab0e7d3bd74d4ec35e958891
d-fin <dfinley5656@gmail.com>
2023-08-05 09:25:28 -0400

    finishing adding unit test project - all tests have been removed and are being store locally.

---
+++ WashEntrance_V1/WashEntrance_Tests/Interface/ISeaMaxDeviceHandlerWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sealevel; 
+
+namespace WashEntrance_Tests.Interface
+{
+    public interface ISeaMAXDeviceHandlerWrapper
+    {
+        byte[] SM_ReadDigitalInputs();
+    }
+
+    public class SeaMAXDeviceHandlerWrapper : ISeaMAXDeviceHandlerWrapper
+    {
+        public SeaMAX device = new SeaMAX();
+
+        public byte[] SM_ReadDigitalInputs(int start, int quantity, byte[] input)
+        {
+            int err = device.SM_ReadDigitalInputs(start, quantity, input);
+
+            return input; 
+        }
+    }
+
+}
---
+++ WashEntrance_V1/WashEntrance_Tests/SeaLevelThreadTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WashEntrance_V1;
+using Moq;
+using Sealevel;
+using WashEntrance_Tests.Interface; 
+
+namespace WashEntrance_Tests
+{
+    [TestClass]
+    public class SeaLevelThreadTests
+    {
+      // deleted all tests. 
+    }
+}
